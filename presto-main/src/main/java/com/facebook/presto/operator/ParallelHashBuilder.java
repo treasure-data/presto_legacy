b82de372514024989da8b7695e6304cc66a983a2
Dongmin Yu <miniway@gmail.com>
2016-03-17 18:31:56 +0900

    Fix missing page memory tracking at parallel build

--- presto-main/src/main/java/com/facebook/presto/operator/ParallelHashBuilder.java
+++ presto-main/src/main/java/com/facebook/presto/operator/ParallelHashBuilder.java
@@ -270,6 +270,11 @@ public class ParallelHashBuilder
                 size += index.getEstimatedSize().toBytes();
             }
 
+            // PagesIndex.addPage doesn't track page memory but blocks are shared and kept
+            for (Block block : page.getBlocks()) {
+                size += block.getRetainedSizeInBytes();
+            }
+
             operatorContext.setMemoryReservation(size);
             operatorContext.recordGeneratedOutput(page.getSizeInBytes(), page.getPositionCount());
         }
